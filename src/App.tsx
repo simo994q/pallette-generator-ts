8e94feb11301bf47fc39ff7d3d5c03e0a61d8228
simo994q <simonkn1@hotmail.com>
2024-01-10 12:15:43 +0100

    color group

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import ColorContextProvider from "./ColorContext"
 import ColorCard from "./Components/ColorCard/ColorCard"
+import ColorGroup from "./Components/ColorGroup/ColorGroup"
 
 function App() {
 
   return (
     <>
       <ColorContextProvider>
-        <ColorCard color={'#343434'} copytext={true}></ColorCard>
+        <ColorCard color={'#343434'} copytext={true} />
+        <ColorGroup>
+            <ColorCard color={'#348fe3'} copytext={true} />
+            <ColorCard color={'#432344'} copytext={true} />
+            <ColorCard color={'#f54347'} copytext={true} />
+        </ColorGroup>
       </ColorContextProvider>
     </>
   )
