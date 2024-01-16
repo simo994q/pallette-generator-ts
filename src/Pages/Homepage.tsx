0a9a8cfea8ea65b152489760df1a79a66fcbbb73
Highog92 <Madsserritslev@gmail.com>
2024-01-16 09:19:18 +0100

    Toasty

--- src/Pages/Homepage.tsx
+++ src/Pages/Homepage.tsx
@@ -1,9 +1,10 @@
-import homepageStyle from '../Pages/Homepage.module.scss'
-import { Button } from "../Components/Button/Buttons"
 import { useContext, useEffect } from 'react'
+import { toast } from 'react-toastify'
 import { ColorContext } from '../ColorContext'
+import { Button } from "../Components/Button/Buttons"
 import ColorCard from '../Components/ColorCardHome/ColorCardHome'
 import ColorGroup from '../Components/ColorGroupHome/ColorGroupHome'
+import homepageStyle from '../Pages/Homepage.module.scss'
 
 export function Homepage({ title }: { title: string }) {
 
@@ -35,6 +36,8 @@ export function Homepage({ title }: { title: string }) {
         setActivePallette(allResultsHex)
         setGeneratedPallette(allResultsHex)
 
+        toast('Your new pallets is here')
+
         localStorage.setItem('generatedPallette', JSON.stringify(allResultsHex))
         localStorage.setItem('activePallette', JSON.stringify(allResultsHex))
         return data;
@@ -44,6 +47,8 @@ export function Homepage({ title }: { title: string }) {
     const saveGeneratedPallette = () => {
         if (localStorage.getItem('userPallettes')) {
             const localPallettesJSON = JSON.parse(localStorage.getItem('userPallettes')!)
+            
+            toast('The Pallettes has been saved')
 
             const doesItExist = localPallettesJSON.some((palette: Array<Array<string>>) => JSON.stringify(palette) === JSON.stringify(generatedPallette))
             console.log(doesItExist);
@@ -83,15 +88,15 @@ export function Homepage({ title }: { title: string }) {
     // ${activePallette[0]}, ${activePallette[1]}, ${activePallette[2]}, ${activePallette[3]}, ${activePallette[4]}
 
     console.log(`-webkit-linear-gradient(${JSON.parse(localStorage.getItem('activePallette')!)[0]}, ${JSON.parse(localStorage.getItem('activePallette')!)[1]}, ${JSON.parse(localStorage.getItem('activePallette')!)[2]}, ${JSON.parse(localStorage.getItem('activePallette')!)[3]}, ${JSON.parse(localStorage.getItem('activePallette')!)[4]})`);
-    
+
     return (
         <section className={homepageStyle.homepage}>
             <div className={homepageStyle.headline}>
-                {localStorage.getItem('activePallette') 
-                ? 
-                <h1 style={{ background: `-webkit-linear-gradient(left, ${JSON.parse(localStorage.getItem('activePallette')!)[0]}, ${JSON.parse(localStorage.getItem('activePallette')!)[1]}, ${JSON.parse(localStorage.getItem('activePallette')!)[2]}, ${JSON.parse(localStorage.getItem('activePallette')!)[3]}, ${JSON.parse(localStorage.getItem('activePallette')!)[4]}) text`}}>{title}</h1>
-                :
-                <h1 style={{ background: `-webkit-linear-gradient(#bab393, #f1e5c1, #d0753c, #5e3326, #2d2a23)`, WebkitBackgroundClip: 'text'}}>{title}</h1>
+                {localStorage.getItem('activePallette')
+                    ?
+                    <h1 style={{ background: `-webkit-linear-gradient(left, ${JSON.parse(localStorage.getItem('activePallette')!)[0]}, ${JSON.parse(localStorage.getItem('activePallette')!)[1]}, ${JSON.parse(localStorage.getItem('activePallette')!)[2]}, ${JSON.parse(localStorage.getItem('activePallette')!)[3]}, ${JSON.parse(localStorage.getItem('activePallette')!)[4]}) text` }}>{title}</h1>
+                    :
+                    <h1 style={{ background: `-webkit-linear-gradient(#bab393, #f1e5c1, #d0753c, #5e3326, #2d2a23)`, WebkitBackgroundClip: 'text' }}>{title}</h1>
                 }
             </div>
 
@@ -114,9 +119,10 @@ export function Homepage({ title }: { title: string }) {
 
 
             <div className={homepageStyle.generateSave}>
-                <Button onClick={() => fetchNewPallette()}>Generate</Button>
-                <Button onClick={() => saveGeneratedPallette()}>Save</Button>
+                    <Button onClick={() => fetchNewPallette()}>Generate</Button>
+                    <Button onClick={() => saveGeneratedPallette()}>Save</Button>
             </div>
-        </section>
+
+        </section >
     )
 }
